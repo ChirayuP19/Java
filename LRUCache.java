fa2f2b9c18a3b7d67c17ff2deeaeb02bdb9bf59e
ChirayuP19 <chirayu.patel1@dcmail.ca>
2025-10-26 15:32:56 -0400

    LRUCache basic java program
    In this program only contain 3 element and whey you put or get new element old element automatically remove from list.

---
+++ LRUCache.java
@@ -0,0 +1,30 @@
+package mapCollection;
+
+import java.util.LinkedHashMap;
+
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+
+	public LRUCache(int capacity) {
+		super(capacity, 0.75f, true);
+		this.capacity = capacity;
+	}
+
+	private int capacity;
+
+	public static void main(String[] args) {
+		// 3 entry when 4 add remove 3st entry
+
+		LRUCache<String, Integer> studentMap = new LRUCache<>(3);
+		studentMap.put("Kavya", 100);
+		studentMap.put("Chirayu", 87);
+		studentMap.put("Neha", 74);
+		studentMap.get("Chirayu");
+		System.out.println(studentMap);
+	}
+
+	@Override
+	protected boolean removeEldestEntry(java.util.Map.Entry<K, V> eldest) {
+		return size() > capacity;
+	}
+
+}
